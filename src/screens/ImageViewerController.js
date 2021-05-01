4764469a397f8551b0a45561d1ed3ea3da28eebf
susri1125 <susri1125@gmail.com>
2021-05-01 14:14:48 +0530

    Profile_Page

--- src/screens/ImageViewerController.js
+++ src/screens/ImageViewerController.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from "./login/Login";
 import Home from './home/Home';
+import Profile from './profile/Profile';
 
 class ImageViewerController extends Component {
 
@@ -16,6 +17,7 @@ class ImageViewerController extends Component {
                 <div>
                     <Route exact path='/' render={(props) => <Login {...props} />} />
                     <Route exact path='/home' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
+                    <Route exact path='/profile' render={(props) => <Profile {...props} baseUrl={this.baseUrl} />} />
                 </div>
             </Router>
         )
